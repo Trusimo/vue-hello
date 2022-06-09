28415942219e9d855be0dbc08c404d21b89e0910
Trusimo <simonetrunfio96@gmail.com>
2022-06-09 16:03:41 +0200

    image not loading :(

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const appVue = new Vue({
     el: "#app",
     data: {
-        messaggioH1: "Primo esercizio con VueJS"
+        messaggioH1: "Primo esercizio con VueJS",
+        immagine: "https://unsplash.com/photos/RP7mZFXz7L0"
     },
 });
